df6e552f3736b5e37990197bbd90de90c86f17e5
Kanoooooo <31366000+Kanoooooo@users.noreply.github.com>
2018-03-15 09:16:39 +0800

    登录场景
    用户名注册

---
+++ Assets/Scripts/ChangeScene/ChangeScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    //获得按钮游戏对象
+    public GameObject m_oSign;
+    public GameObject m_oLogin;
+    public GameObject m_oStandAlone;
+    public GameObject m_oExit;
+	
+	// Update is called once per frame
+	void Update () {
+        //点击对应按钮触发对应事件
+        UIEventListener.Get(m_oSign).onClick = Sign;
+        UIEventListener.Get(m_oLogin).onClick = Login;
+        UIEventListener.Get(m_oStandAlone).onClick = StandAlone;
+        UIEventListener.Get(m_oExit).onClick = Exit;
+	}
+
+    //点击注册按钮
+    void Sign(GameObject button)
+    {
+
+    }
+    //点击登录按钮
+    void Login(GameObject button)
+    {
+
+    }
+    //点击单人游戏按钮
+    void StandAlone(GameObject button)
+    {
+        SceneManager.LoadScene("Chess");
+    }
+    //点击退出游戏按钮
+    void Exit(GameObject button)
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Test/TestPicture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPicture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+//    IEnumerator LoadTexture(string name)
+//    {
+//        string Str = null;
+//#if UNITY_ANDROID
+//        Str = "file://" + name;
+//#elif UNITY_IPHONE
+//            Str = "file:///" + name;
+//#else
+//            Str = "file://" + name;
+//#endif
+//        PhotoPath = name;
+//        WWW www = new WWW(Str);
+//        while (!www.isDone) { }
+//        yield return www;
+//        mTextrue = www.texture;
+//        if (mTextrue != null)
+//        {
+//            mImage.texture = mTextrue;
+//        }
+//    }
+}
